30e9b7e229c5d8aa81583d37226be8e69a102e45
cxelva <166345539+cxelva@users.noreply.github.com>
2026-06-01 15:33:04 +0800

    Add files via upload

---
+++ js/2400.js
@@ -0,0 +1,6 @@
+[
+{
+  "name": "2500多部恐怖电影合集",
+  "url": "https://d.kstore.dev/download/8975/2500多部恐怖电影合集.txt&&&https://ts1.tc.mm.bing.net/th/id/R-C.97e4c2c8012b360272641de67410af0a?rik=9RrCEDmpY8qvlQ&riu=http%3a%2f%2fpuui.qpic.cn%2fvcover_vt_pic%2f0%2f6nszx1gt6yvu3b61558324760%2f0&ehk=BazSKIAB1FBuixtbcgipkct61eI0GGc8kj1rNI9cfBo%3d&risl=&pid=ImgRaw&r=0"
+    }
+    ]
